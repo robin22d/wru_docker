054434282f0acae61f42c8ad5360c615326fbf06
Python Virtualbox <python@virtualbox>
2017-04-29 19:53:02 +0100

    Added docker compose to install script and made it executable

--- scripts/docker_install.sh
+++ scripts/docker_install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-sudo apt-get -y update
+sudo apt -y update
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
-sudo apt-get -y update
-sudo apt-get -y install docker-engine
+sudo apt -y update
+sudo apt -y install docker-engine
 sudo usermod -aG docker $(whoami)
-
+sudo apt -y install python-pip
+pip install --user docker-compose
 
 echo "You must log out to complete installation process ..."
+
